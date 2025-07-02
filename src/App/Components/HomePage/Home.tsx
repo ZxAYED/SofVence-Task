6fcbb4cf96aae648a450ab88622093f7fae95ac2
robiul@hasan <robihasan093@gmail.com>
2025-07-02 12:09:47 +0600

    create searchBar, joinUs, thambnail section

--- src/App/Components/HomePage/Home.tsx
+++ src/App/Components/HomePage/Home.tsx
@@ -1,10 +1,16 @@
+import SearchBar from "@/App/Shared/SearchBar";
+import JoinUs from "./JoinUs";
 import TopManufacturer from "./TopManufacturer";
+import Thambnail from "@/App/Shared/Thambnail";
 
 const Home = () => {
 return (
   <>
     <div>Welcome to SoftVence</div>
+    <SearchBar />
+    <Thambnail />
     <TopManufacturer />
+    <JoinUs />
   </>
 );
 };
--- src/App/Components/HomePage/TopManufacturer.tsx
+++ src/App/Components/HomePage/TopManufacturer.tsx
@@ -59,12 +59,12 @@ const manufacturers =[
 ]
 ;
 
-export default function TopRatedManufacturers() {
+const TopRatedManufacturers = () => {
   return (
-    <section className="py-10 bg-gray-50 dark:bg-gray-900">
-      <div className="container mx-auto px-4 text-center">
-        <h2 className="text-3xl font-bold mb-6">Top Rated Manufacturers</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <section className="py-10 max-w-[1520px] !mx-auto">
+      <div className="mx-auto px-4 text-center">
+        <h2 className="text-2xl md:text-3xl font-bold mb-6 text-start">Top Rated Manufacturers</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 lg:gap-8">
           {manufacturers.map((mfg) => (
             <ManufacturerCard
               key={mfg.id}
@@ -80,3 +80,6 @@ export default function TopRatedManufacturers() {
     </section>
   );
 }
+
+
+export default TopRatedManufacturers;
